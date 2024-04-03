8911e9d942d7e77a9448e08c83a489ae2bd771c0
venus1344 <dmez777@gmail.com>
2024-04-03 23:27:43 +0300

    initial commit

---
+++ src/main/java/org/rdxi/oyatubeti/OyaTubetiApplication.java
@@ -0,0 +1,13 @@
+package org.rdxi.oyatubeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyaTubetiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OyaTubetiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rdxi/oyatubeti/OyaTubetiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rdxi.oyatubeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyaTubetiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
